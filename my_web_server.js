b592d70216b3fe10b4704d611c5960833b6fc9aa
dklounge <daviddarden11@gmail.com>
2013-10-15 17:42:02 -0700

    express

--- my_web_server.js
+++ my_web_server.js
@@ -1,2 +1,12 @@
 // HTTP server example
 // http://blog.modulus.io/absolute-beginners-guide-to-nodejs
+var http = require('http');
+
+http.createServer(function(err, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('test text\n');
+}).listen(8080);
+
+console.log('running on port 8080');
+
+// next step might be to install express
